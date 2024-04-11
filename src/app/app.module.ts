588f3b0a3b2e16b4b0d6c6f1eaa4c6df03a66577
iamnga <51619515+iamnga@users.noreply.github.com>
2024-04-11 08:10:18 +0700

    Update UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, TooltipModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
